b8c34483e8ab18d1d4e2bea37ecf80195017f1f4
mokemoko <mokemoko331@gmail.com>
2021-11-28 18:07:07 +0900

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import * as reactPlugin from 'vite-plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig(({ mode }) => {
   return {
     jsx: 'react',
-    plugins: [reactPlugin],
+    plugins: [react()],
+    server: {
+      open: true,
+    },
     build: {
       rollupOptions: {
         plugins: [
-          mode === 'analyze' && visualizer({
+          visualizer({
             open: true,
             filename: 'docs/stats.html',
             gzipSize: true,
