f6d4a5be84d4d0431fe2c1223d1a6a0aaa756979
mokemoko <mokemoko331@gmail.com>
2021-12-01 17:05:27 +0900

    use recoil-persist

--- src/services/state.ts
+++ src/services/state.ts
@@ -1,7 +1,11 @@
 import { atom } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
 import { User } from '../models/user'
+
+const { persistAtom } = recoilPersist()
 
 export const userState = atom<User | null>({
   key: 'userState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 })
